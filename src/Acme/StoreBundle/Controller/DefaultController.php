4a725d9eabb2edd0a923da327b60ce344da628ae
Nattaphat K <nattaphat@haii.or.th>
2013-03-22 17:10:24 +0700

    DQL

--- src/Acme/StoreBundle/Controller/DefaultController.php
+++ src/Acme/StoreBundle/Controller/DefaultController.php
@@ -39,8 +39,83 @@ class DefaultController extends Controller
                     'No product found for id '.$id
             );
         }
-    
+        
+//         // query by the primary key (usually "id")
+//         $product = $repository->find($id);
+        
+//         // dynamic method names to find based on a column value
+//         $product = $repository->findOneById($id);
+//         $product = $repository->findOneByName('foo');
+        
+//         // find *all* products
+//         $products = $repository->findAll();
+        
+//         // find a group of products based on an arbitrary column value
+//         $products = $repository->findByPrice(19.99);
+        
+//         // query for one product matching be name and price
+//         $product = $repository->findOneBy(array('name' => 'foo', 'price' => 19.99));
+        
+//         // query for all products matching the name, ordered by price
+//         $product = $repository->findBy(
+//                 array('name' => 'foo'),
+//                 array('price' => 'ASC')
+//         );
+        
         // ... do something, like pass the $product object into a template
         return new Response('Product id will show is:'.$product->getName());
+    }
+    
+    public function updateAction($id)
+    {
+        $em = $this->getDoctrine()->getManager();
+        $product = $em->getRepository('AcmeStoreBundle:Product')->find($id);
+    
+        if (!$product) {
+            throw $this->createNotFoundException(
+                    'No product found for id '.$id
+            );
+        }
+    
+        $product->setName('New product name!');
+        $em->flush();
+        
+//for delete
+//         $em->remove($product);
+//         $em->flush();
+
+//Querying for Objects with DQL
+//         $em = $this->getDoctrine()->getManager();
+//         $query = $em->createQuery(
+//                 'SELECT p FROM AcmeStoreBundle:Product p WHERE p.price > :price ORDER BY p.price ASC'
+//         )->setParameter('price', '19.99');
+
+//  The getResult() method returns an array of results. If you're querying for just one object, you can use the getSingleResult() method instead:      
+//         $products = $query->getResult();
+
+//method throws
+//         $query = $em->createQuery('SELECT ...')
+//         ->setMaxResults(1);
+        
+//         try {
+//             $product = $query->getSingleResult();
+//         } catch (\Doctrine\Orm\NoResultException $e) {
+//             $product = null;
+//         }
+
+//  Using Doctrine's Query Builder      
+//         $repository = $this->getDoctrine()
+//         ->getRepository('AcmeStoreBundle:Product');
+        
+//         $query = $repository->createQueryBuilder('p')
+//         ->where('p.price > :price')
+//         ->setParameter('price', '19.99')
+//         ->orderBy('p.price', 'ASC')
+//         ->getQuery();
+        
+//         $products = $query->getResult();
+        
+        return new Response('Product id will show is:'.$product->getName());
+        //return $this->redirect($this->generateUrl('homepage'));
     }
 }
--- src/Acme/StoreBundle/Entity/Product.php
+++ src/Acme/StoreBundle/Entity/Product.php
@@ -35,7 +35,13 @@ class Product
      * @ORM\Column(type="text")
      */
     protected $description;
-
+    
+    /**
+     * @ORM\ManyToOne(targetEntity="Category", inversedBy="products")
+     * @ORM\JoinColumn(name="category_id", referencedColumnName="id")
+     */
+    protected $category;
+    
     /**
      * Get id
      *
@@ -113,5 +119,28 @@ class Product
     public function getDescription()
     {
         return $this->description;
+    }
+
+    /**
+     * Set category
+     *
+     * @param \Acme\StoreBundle\Entity\Category $category
+     * @return Product
+     */
+    public function setCategory(\Acme\StoreBundle\Entity\Category $category = null)
+    {
+        $this->category = $category;
+    
+        return $this;
+    }
+
+    /**
+     * Get category
+     *
+     * @return \Acme\StoreBundle\Entity\Category 
+     */
+    public function getCategory()
+    {
+        return $this->category;
     }
 }
